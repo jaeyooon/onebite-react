e45d0e8a7861b4f8da337779bfded10f828ff39b
jaeyooon <jycho0208@gmail.com>
2024-11-14 22:14:51 +0900

    add section3

---
+++ section03/src/math.js
@@ -0,0 +1,13 @@
+// math 모듈
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export default function multiply(a, b) {
+  return a * b;
+}   // math 모듈을 대표하는 기본값이 됨.
