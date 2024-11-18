ea7909b44d881a42b5f7f774d3fcb50d1db6a88c
jaeyooon <jycho0208@gmail.com>
2024-11-18 23:51:36 +0900

    add section5 > state & props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,27 +1,15 @@
 import './App.css'
-import Button from './components/Button'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Main from './components/Main'
+import Bulb from './components/Bulb';
+import Counter from './components/Counter';
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  }
 
   return (
     <>
-      <Button {...buttonProps} />   {/* spread 연산자 이용 */}
-      <Button text={ "카페"}/>
-      <Button text={"블로그"}>
-        <Header />
-      </Button>
+      <Bulb />
+      <Counter />
     </>
-  )
-}
+  );
+};
 
 export default App
