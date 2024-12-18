db11ffd2ff11c644aa4190ec88f759aa848f828e
jaeyooon <jycho0208@gmail.com>
2024-12-18 23:48:50 +0900

    add section8 > TodoList UI 구현 & 투두 추가 기능

---
+++ section08/src/components/Editor.jsx
@@ -0,0 +1,41 @@
+import "./Editor.css";
+import { useState, useRef } from "react";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {    // Enter 키 눌렀을때 추가되도록
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content)
+    setContent("");
+  };  
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  )
+};
+
+export default Editor;
